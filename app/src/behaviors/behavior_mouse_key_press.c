6be9df4cb66046dcd05d83682eb58c996dd97d0d
Klesh Wong <klesh@qq.com>
2025-03-04 18:29:43 +0800

    feat: settinging mouse button using ZMK Studio

--- app/src/behaviors/behavior_mouse_key_press.c
+++ app/src/behaviors/behavior_mouse_key_press.c
@@ -19,6 +19,48 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
+#if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+
+static const struct behavior_parameter_value_metadata param_values[] = {
+    {
+        .display_name = "Button 1",
+        .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,
+        .value = MB1,
+    },
+    {
+        .display_name = "Button 2",
+        .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,
+        .value = MB2,
+    },
+    {
+        .display_name = "Button 3",
+        .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,
+        .value = MB3,
+    },
+    {
+        .display_name = "Button 4",
+        .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,
+        .value = MB4,
+    },
+    {
+        .display_name = "Button 5",
+        .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,
+        .value = MB5,
+    },
+};
+
+static const struct behavior_parameter_metadata_set param_metadata_set[] = {{
+    .param1_values = param_values,
+    .param1_values_len = ARRAY_SIZE(param_values),
+}};
+
+static const struct behavior_parameter_metadata metadata = {
+    .sets_len = ARRAY_SIZE(param_metadata_set),
+    .sets = param_metadata_set,
+};
+
+#endif
+
 static int behavior_mouse_key_press_init(const struct device *dev) { return 0; };
 
 static void process_key_state(const struct device *dev, int32_t val, bool pressed) {
@@ -49,7 +91,12 @@ static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
 }
 
 static const struct behavior_driver_api behavior_mouse_key_press_driver_api = {
-    .binding_pressed = on_keymap_binding_pressed, .binding_released = on_keymap_binding_released};
+    .binding_pressed = on_keymap_binding_pressed,
+    .binding_released = on_keymap_binding_released,
+#if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+    .parameter_metadata = &metadata,
+#endif // IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+};
 
 #define MKP_INST(n)                                                                                \
     BEHAVIOR_DT_INST_DEFINE(n, behavior_mouse_key_press_init, NULL, NULL, NULL, POST_KERNEL,       \
